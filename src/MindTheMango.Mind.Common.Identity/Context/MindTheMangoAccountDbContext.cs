29855143665386cc8ad5f8a0e59ff7e9ce284a6f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-18 23:47:36 +0100

    Implemented account context

--- src/MindTheMango.Mind.Common.Identity/Context/MindTheMangoAccountDbContext.cs
+++ src/MindTheMango.Mind.Common.Identity/Context/MindTheMangoAccountDbContext.cs
@@ -1,10 +1,17 @@
 using System;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace MindTheMango.Mind.Common.Identity.Context
 {
     public class MindTheMangoAccountDbContext : IdentityDbContext<Account, AccountRole, Guid>
     {
+        public MindTheMangoAccountDbContext()
+        {
+        }
         
+        public MindTheMangoAccountDbContext(DbContextOptions<MindTheMangoAccountDbContext> options) : base(options)
+        {
+        }
     }
 }
