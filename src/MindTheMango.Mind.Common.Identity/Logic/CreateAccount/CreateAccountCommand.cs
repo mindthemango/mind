2105c0b3d0b0fbe4c95c2b6d6e8db70231240cfb
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-19 15:24:10 +0100

    Implemented handler for creating account

--- src/MindTheMango.Mind.Common.Identity/Logic/CreateAccount/CreateAccountCommand.cs
+++ src/MindTheMango.Mind.Common.Identity/Logic/CreateAccount/CreateAccountCommand.cs
@@ -1,9 +1,10 @@
 using System;
 using MindTheMango.Mind.Common.Request;
+using MindTheMango.Mind.Common.Result;
 
 namespace MindTheMango.Mind.Common.Identity.Logic.CreateAccount
 {
-    public class CreateAccountCommand : Request<Guid>
+    public class CreateAccountCommand : Request<Result<Guid>>
     {
         public string Email { get; set; }
         public string Username { get; set; }
---
+++ src/MindTheMango.Mind.Common.Identity/Logic/CreateAccount/CreateAccountCommandHandler.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using MindTheMango.Mind.Common.Request;
+using MindTheMango.Mind.Common.Result;
+
+namespace MindTheMango.Mind.Common.Identity.Logic.CreateAccount
+{
+    public class CreateAccountCommandHandler : RequestHandler<CreateAccountCommand, Result<Guid>>
+    {
+        private readonly UserManager<Account> _userManager;
+
+        public CreateAccountCommandHandler(ILogger<RequestHandler<CreateAccountCommand, Result<Guid>>> logger, UserManager<Account> userManager) : base(logger)
+        {
+            _userManager = userManager;
+        }
+
+        public override async Task<Result<Guid>> Handle(CreateAccountCommand request, CancellationToken cancellationToken)
+        {
+            var account = new Account()
+            {
+                Id = Guid.NewGuid(),
+                Email = request.Email,
+                UserName = request.Username
+            };
+
+            var result = await _userManager.CreateAsync(account, request.Password);
+
+            if (!result.Succeeded)
+            {
+                var error = result.Errors.FirstOrDefault();
+                
+                if (error != null)
+                {
+                    return Result<Guid>.Fail(error.Code, new List<string> {error.Description});
+                }
+            }
+            
+            return Result<Guid>.Success(account.Id);
+
+        }
+    }
+}
