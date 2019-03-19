6cec0e772d238f6dbd3c17cec6c4668a01080b00
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-19 16:15:43 +0100

    Added CreateAccount validator and FluentValidation DI in Common.Identity

--- src/MindTheMango.Mind.Common.Identity/Configuration/DependencyInjectionExtension.cs
+++ src/MindTheMango.Mind.Common.Identity/Configuration/DependencyInjectionExtension.cs
@@ -1,8 +1,12 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MindTheMango.Mind.Common.Identity.Context;
+using MindTheMango.Mind.Common.Identity.Logic.CreateAccount;
 
 namespace MindTheMango.Mind.Common.Identity.Configuration
 {
@@ -12,6 +16,8 @@ namespace MindTheMango.Mind.Common.Identity.Configuration
         {
             services.AddDatabaseContext(configuration);
             services.AddIdentityCore(configuration);
+            services.AddMediatRHandlers(configuration);
+            services.AddCustomValidation(configuration);
             
             return services;
         }
@@ -32,7 +38,7 @@ namespace MindTheMango.Mind.Common.Identity.Configuration
             var identityBuilder = services.AddIdentityCore<Account>(o =>
             {
                 o.Password.RequireDigit = true;
-                o.Password.RequireLowercase = false;
+                o.Password.RequireLowercase = true;
                 o.Password.RequireUppercase = false;
                 o.Password.RequireNonAlphanumeric = false;
                 o.Password.RequiredLength = 8;
@@ -41,6 +47,22 @@ namespace MindTheMango.Mind.Common.Identity.Configuration
             identityBuilder = new IdentityBuilder(identityBuilder.UserType, typeof(AccountRole), identityBuilder.Services);
 
             identityBuilder.AddEntityFrameworkStores<MindTheMangoAccountDbContext>().AddDefaultTokenProviders();
+
+            return services;
+        }
+        
+        private static IServiceCollection AddMediatRHandlers(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(typeof(CreateAccountCommand).GetTypeInfo().Assembly);
+
+            return services;
+        }
+
+        private static IServiceCollection AddCustomValidation(this IServiceCollection services, IConfiguration configuration)
+        {
+            AssemblyScanner.FindValidatorsInAssemblyContaining<CreateAccountCommandValidator>().ForEach(result => {
+                services.AddTransient(result.InterfaceType, result.ValidatorType);
+            });
 
             return services;
         }
