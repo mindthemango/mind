24c7d523fa406bdecf72c433d8bf9832bec07e50
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-19 16:51:15 +0100

    Added UnitOfWork interface

---
+++ src/MindTheMango.Mind.Persistence.Contract/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using MindTheMango.Mind.Persistence.Contract.Repository;
+
+namespace MindTheMango.Mind.Persistence.Contract
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; set; }
+        
+        Task SaveAsync();
+    }
+}
