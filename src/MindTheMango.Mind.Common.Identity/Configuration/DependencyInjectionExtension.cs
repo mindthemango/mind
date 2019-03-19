bde3caf0ab9a24029c36d9f4a43dc2f37ca8056e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-19 09:26:38 +0100

    Implementing identity IoC, added identity service

--- src/MindTheMango.Mind.Common.Identity/Configuration/DependencyInjectionExtension.cs
+++ src/MindTheMango.Mind.Common.Identity/Configuration/DependencyInjectionExtension.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,12 +8,39 @@ namespace MindTheMango.Mind.Common.Identity.Configuration
 {
     public static class DependencyInjectionExtension
     {
+        public static IServiceCollection AddIdentityDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDatabaseContext(configuration);
+            services.AddIdentityCore(configuration);
+            
+            return services;
+        }
+        
         private static IServiceCollection AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<MindTheMangoAccountDbContext>(options =>
             {
                 options.UseNpgsql(configuration.GetConnectionString("MindTheMangoAccountDatabase"));
             });
+
+            return services;
+        }
+        
+        private static IServiceCollection AddIdentityCore(this IServiceCollection services, IConfiguration configuration)
+        {
+            // configure identity options
+            var identityBuilder = services.AddIdentityCore<Account>(o =>
+            {
+                o.Password.RequireDigit = true;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 8;
+            });
+            
+            identityBuilder = new IdentityBuilder(identityBuilder.UserType, typeof(AccountRole), identityBuilder.Services);
+
+            identityBuilder.AddEntityFrameworkStores<MindTheMangoAccountDbContext>().AddDefaultTokenProviders();
 
             return services;
         }
