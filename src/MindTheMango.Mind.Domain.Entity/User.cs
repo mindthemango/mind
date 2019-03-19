990eb762f26a2897d33dba9e8d9fa5f1efefbb21
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-19 12:31:14 +0100

    Created user entity

---
+++ src/MindTheMango.Mind.Domain.Entity/User.cs
@@ -0,0 +1,14 @@
+namespace MindTheMango.Mind.Domain.Entity
+{
+    public class User
+    {
+        /// <summary>
+        /// Name of the user.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Surname of the user.
+        /// </summary>
+        public string Surname { get; set; }
+    }
+}
