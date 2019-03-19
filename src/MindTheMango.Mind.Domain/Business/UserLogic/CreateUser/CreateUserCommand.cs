0bc8e7c2f26dfc453a51d38f6568a58df5c60801
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-19 16:28:25 +0100

    Created command for creating user in domain

---
+++ src/MindTheMango.Mind.Domain/Business/UserLogic/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+using System;
+using MindTheMango.Mind.Common.Request;
+using MindTheMango.Mind.Common.Result;
+
+namespace MindTheMango.Mind.Domain.Business.UserLogic.CreateUser
+{
+    public class CreateUserCommand : Request<Result<Guid>>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
