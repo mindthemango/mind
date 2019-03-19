f5cf1f3b5fb7db3b75d8a136b22bb649c6284e78
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-19 13:26:01 +0100

    Implemented CreateAccountCommand

--- src/MindTheMango.Mind.Common.Identity/Logic/CreateAccount/CreateAccountCommand.cs
+++ src/MindTheMango.Mind.Common.Identity/Logic/CreateAccount/CreateAccountCommand.cs
@@ -1,7 +1,12 @@
+using System;
+using MindTheMango.Mind.Common.Request;
+
 namespace MindTheMango.Mind.Common.Identity.Logic.CreateAccount
 {
-    public class CreateAccountCommand
+    public class CreateAccountCommand : Request<Guid>
     {
-        
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
