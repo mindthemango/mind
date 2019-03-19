59014b6636a3942a766f161786813fcddd6d5814
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-19 17:19:07 +0100

    Implemented CreateUserCommand handler

--- src/MindTheMango.Mind.Domain/Business/UserLogic/CreateUser/CreateUserCommandHandler.cs
+++ src/MindTheMango.Mind.Domain/Business/UserLogic/CreateUser/CreateUserCommandHandler.cs
@@ -4,18 +4,38 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using MindTheMango.Mind.Common.Request;
 using MindTheMango.Mind.Common.Result;
+using MindTheMango.Mind.Domain.Entity;
+using MindTheMango.Mind.Persistence.Contract;
 
 namespace MindTheMango.Mind.Domain.Business.UserLogic.CreateUser
 {
     public class CreateUserCommandHandler : RequestHandler<CreateUserCommand, Result<Guid>>
     {
-        public CreateUserCommandHandler(ILogger<RequestHandler<CreateUserCommand, Result<Guid>>> logger) : base(logger)
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateUserCommandHandler(ILogger<RequestHandler<CreateUserCommand, Result<Guid>>> logger, IUnitOfWork unitOfWork) : base(logger)
         {
+            _unitOfWork = unitOfWork;
         }
 
-        public override Task<Result<Guid>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        public override async Task<Result<Guid>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var user = new User()
+            {
+                Id = request.Id,
+                Name = request.Name,
+                Surname = request.Surname,
+                RegistrationDate = request.Timestamp,
+                Timestamp = request.Timestamp
+            };
+            
+            await _unitOfWork.UserRepository.Insert(user);
+
+            await _unitOfWork.SaveAsync();
+            
+            Logger.LogTrace($"Created new user {user.Id} ({user.Name})");
+            
+            return Result<Guid>.Success(user.Id);
         }
     }
 }
