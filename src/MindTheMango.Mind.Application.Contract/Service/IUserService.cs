abb92d0127d21f7f10b3ec1c87e8c2e8c805ccbd
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-19 12:50:32 +0100

    Added find method to user service (not yet implemented)

--- src/MindTheMango.Mind.Application.Contract/Service/IUserService.cs
+++ src/MindTheMango.Mind.Application.Contract/Service/IUserService.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Threading.Tasks;
+using MindTheMango.Mind.Application.Contract.Dto;
 using MindTheMango.Mind.Common.Result;
 
 namespace MindTheMango.Mind.Application.Contract.Service
 {
     public interface IUserService
     {
+        Task<Result<UserDto>> Find(Guid id);
+
         Task<Result<Guid>> Create(string name, string surname, string username, string email, string password);
     }
 }
--- src/MindTheMango.Mind.Application.Implementation/Service/UserService.cs
+++ src/MindTheMango.Mind.Application.Implementation/Service/UserService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using MindTheMango.Mind.Application.Contract.Dto;
 using MindTheMango.Mind.Application.Contract.Service;
 using MindTheMango.Mind.Common.Result;
 
@@ -7,6 +8,11 @@ namespace MindTheMango.Mind.Application.Implementation.Service
 {
     public class UserService : IUserService
     {
+        public Task<Result<UserDto>> Find(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<Result<Guid>> Create(string name, string surname, string username, string email, string password)
         {
             throw new NotImplementedException();
