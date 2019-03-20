582d4d101f49fa10d1a1f351ec368a86fd4d5e5e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-20 22:12:59 +0100

    Updated delete account command

--- src/MindTheMango.Mind.Common.Identity/Logic/DeleteAccount/DeleteAccountCommand.cs
+++ src/MindTheMango.Mind.Common.Identity/Logic/DeleteAccount/DeleteAccountCommand.cs
@@ -1,9 +1,10 @@
 using System;
 using MindTheMango.Mind.Common.Request;
+using MindTheMango.Mind.Common.Result;
 
 namespace MindTheMango.Mind.Common.Identity.Logic.DeleteAccount
 {
-    public class DeleteAccountCommand : Request<Guid>
+    public class DeleteAccountCommand : Request<Result<Guid>>
     {
         public Guid Id { get; set; }
     }
