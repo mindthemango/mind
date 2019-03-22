a330f37c3c2d1362801129ae4b82c3d38e0c8cc7
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-03-22 22:50:52 +0100

    Created Mind.Application DI

---
+++ src/MindTheMango.Mind.Application.Implementation/Configuration/DependencyInjectionExtension.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MindTheMango.Mind.Application.Implementation.Service;
+using Scrutor;
+
+namespace MindTheMango.Mind.Application.Implementation.Configuration
+{
+    public static class DependencyInjectionExtension
+    {
+        public static IServiceCollection AddApplicationDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            return services;
+        }
+        
+        private static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Scan(scan => scan
+                .FromAssemblyOf<UserService>()
+                .AddClasses(classes => classes.Where(c => c.Name.EndsWith("Service")))
+                .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+            return services;
+        }
+    }
+}
