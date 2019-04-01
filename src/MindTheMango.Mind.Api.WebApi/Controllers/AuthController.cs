834847f5cde88bbf7ed6b67e49a1c864a6ee2094
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-01 18:24:28 +0200

    Created auth controller

---
+++ src/MindTheMango.Mind.Api.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MindTheMango.Mind.Api.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        
+    }
+}
