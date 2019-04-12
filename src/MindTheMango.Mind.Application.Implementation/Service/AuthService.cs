7463d83433bf487255a3239458d8b0102c67983e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-12 12:51:15 +0200

    re-implemented signin method of auth service

--- src/MindTheMango.Mind.Application.Implementation/Service/AuthService.cs
+++ src/MindTheMango.Mind.Application.Implementation/Service/AuthService.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.JsonWebTokens;
 using MindTheMango.Mind.Application.Contract.Service;
 using MindTheMango.Mind.Common.Identity;
 using MindTheMango.Mind.Common.Result;
@@ -13,38 +15,47 @@ namespace MindTheMango.Mind.Application.Implementation.Service
     public class AuthService : IAuthService
     {
         protected readonly UserManager<Account> AccountManager;
-        protected readonly SignInManager<Account> SignInManager;
         protected readonly ILogger<AuthService> Logger;
         
-        public AuthService(ILogger<AuthService> logger, UserManager<Account> accountManager, SignInManager<Account> signInManager)
+        public AuthService(ILogger<AuthService> logger, UserManager<Account> accountManager)
         {
             Logger = logger;
             AccountManager = accountManager;
-            SignInManager = signInManager;
         }
 
-        public async Task<Result<ClaimsPrincipal>> SignIn(string email, string password)
+        public async Task<Result<ClaimsIdentity>> SignIn(string email, string password)
         {
             if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
             {
-                return Result<ClaimsPrincipal>.ValidationFailure(new List<string> {"username/password must not be empty."});
+                return Result<ClaimsIdentity>.ValidationFailure(new List<string> {"username/password must not be empty."});
             }
             
             var account = await AccountManager.FindByEmailAsync(email);            
 
             if (account == null)
             {
-                return Result<ClaimsPrincipal>.NotFound(new List<string> {"username/password combination is wrong or the account does not exists."});
+                return Result<ClaimsIdentity>.NotFound(new List<string> {"username/password combination is wrong or the account does not exists."});
             }
             
             if (!await AccountManager.CheckPasswordAsync(account, password))
             {
-                return Result<ClaimsPrincipal>.NotFound(new List<string> {"username/password combination is wrong or the account does not exists."});
+                return Result<ClaimsIdentity>.NotFound(new List<string> {"username/password combination is wrong or the account does not exists."});
             }
-            
-            var claims = await SignInManager.CreateUserPrincipalAsync(account);
 
-            return Result<ClaimsPrincipal>.Success(claims);
+            var claims = await GetClaimsIdentity(account);
+
+            return Result<ClaimsIdentity>.Success(claims);
+        }
+
+        protected async Task<ClaimsIdentity> GetClaimsIdentity(Account account)
+        {
+            var claims = await AccountManager.GetClaimsAsync(account);
+            
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, account.Email));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, account.Id.ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.UniqueName, account.UserName));
+            
+            return new ClaimsIdentity(new GenericIdentity(account.UserName), claims);
         }
     }
 }
