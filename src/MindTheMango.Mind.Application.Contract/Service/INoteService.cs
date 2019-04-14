aa3c6b031b4cb464908b96e92f5e77061e257728
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-14 16:04:47 +0200

    Created note service interface

---
+++ src/MindTheMango.Mind.Application.Contract/Service/INoteService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MindTheMango.Mind.Application.Contract.Dto;
+using MindTheMango.Mind.Common.Result;
+
+namespace MindTheMango.Mind.Application.Contract.Service
+{
+    public interface INoteService
+    {
+        Task<Result<NoteDto>> Find(Guid userId, Guid noteId, CancellationToken cancellationToken);
+        Task<Result<IList<NoteDto>>> FindAll(Guid userId, CancellationToken cancellationToken);
+        Task<Result<Guid>> Create(Guid userId, string title, string content);
+        Task<Result> Delete(Guid userId, Guid noteId);
+    }
+}
