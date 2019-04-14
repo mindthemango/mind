31a7fe88e43fd07aed93c2d1f9bc4f0e623c2b05
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-14 16:20:44 +0200

    Added constructor to create note command

--- src/MindTheMango.Mind.Domain/Business/Notes/CreateNote/CreateNoteCommand.cs
+++ src/MindTheMango.Mind.Domain/Business/Notes/CreateNote/CreateNoteCommand.cs
@@ -1,12 +1,21 @@
 using System;
 using MindTheMango.Mind.Common.Request;
 using MindTheMango.Mind.Common.Result;
+using MindTheMango.Mind.Domain.Entity;
 
 namespace MindTheMango.Mind.Domain.Business.Notes.CreateNote
 {
     public class CreateNoteCommand : Request<Result<Guid>>
     {
+        public Guid UserId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
+
+        public CreateNoteCommand(Guid userId, string title, string content)
+        {
+            UserId = userId;
+            Title = title;
+            Content = content;
+        }
     }
 }
