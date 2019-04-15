d3c4e1ed0f5fa5d8f30a3c2d4a4c556c6947074f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-15 20:11:52 +0200

    Corrected DbContext in note repository

--- src/MindTheMango.Mind.Persistence.Implementation/Repository/NoteRepository.cs
+++ src/MindTheMango.Mind.Persistence.Implementation/Repository/NoteRepository.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using MindTheMango.Mind.Domain.Entity;
 using MindTheMango.Mind.Persistence.Contract.Repository;
+using MindTheMango.Mind.Persistence.Implementation.Context;
 
 namespace MindTheMango.Mind.Persistence.Implementation.Repository
 {
     public class NoteRepository : GenericRepository<Note>, INoteRepository
     {
-        public NoteRepository(DbContext dbContext) : base(dbContext)
+        public NoteRepository(MindTheMangoDbContext dbContext) : base(dbContext)
         {
         }
     }
