42f876c3f15b031417337ca3c8e225395a3b15c9
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-15 18:06:37 +0200

    Added model for creating notes

---
+++ src/MindTheMango.Mind.Api.WebApi/Model/Note/CreateNoteModel.cs
@@ -0,0 +1,8 @@
+namespace MindTheMango.Mind.Api.WebApi.Model.Note
+{
+    public class CreateNoteModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
